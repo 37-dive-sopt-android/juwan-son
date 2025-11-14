cf279c60c386fbf67d3f6d9cc304f870ce02fd5e
Son Juwan <wndhks171@naver.com>
2025-11-14 22:23:58 +0900

    week4: hilt 적용

---
+++ app/src/main/java/com/sopt/dive/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.sopt.dive.di
+
+import com.sopt.dive.data.repository.AuthRepositoryImpl
+import com.sopt.dive.domain.repository.AuthRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl,
+    ): AuthRepository
+}
